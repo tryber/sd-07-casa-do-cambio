928c3424efa1451ecc92b811ea1160436d979cbc
Gabriel Oliva <gfpoliva@gmail.com>
2020-10-23 16:03:37 -0300

    Requisição na api rates finalizada

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+  setupEventHandlers();
+}
+
+const setupEventHandlers = () => {
+  const searchButton = document.querySelector('#search-button');
+  searchButton.addEventListener('click', handleSearchEvent);
+}
+
+const handleSearchEvent = () => {
+  const currency = document.querySelector('#currency-input').value;
+  
+  if (currency === '') {
+    showAlert('A moeda deve ser informada');
+  } else {
+    fetchCurrency(currency);
+  }
+}
+
+const showAlert = (message) => {
+  window.alert(message);
+}
+
+const fetchCurrency = (currency) => {
+  const endpoint = `https://api.ratesapi.io/api/latest?base=${currency}`;
+
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((object) => {
+      handleRates(object.rates);
+    })
+}
+
+const handleRates = (rates) => {
+  console.log(rates);
+}
